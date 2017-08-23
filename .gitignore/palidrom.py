50e7bac851a4f29f54054168a6c019b2f8fd5e46
rorogant70 <ronagurl19@gmail.com>
2017-08-23 10:06:35 -0400

    Update and rename .gitignore to palidrom.py

---
+++ .gitignore/palidrom.py
@@ -0,0 +1,19 @@
+food = ['burgers', 'fries', 'soda']
+
+food.reverse();
+print "List : ", food
+
+names = ['H' 'A' 'N' 'N' 'A' 'H']
+
+names.reverse();
+print "List : ", names
+
+input_string = "hannah"
+rev_string = reversed(input_string)
+if list (input_string) == list(rev_string):
+    print ("Palindrom")
+else: print('not')
+
+sentence = ["Borrow" "or" "roB?"]
+sentence.reverse();
+print "List : ", sentence
